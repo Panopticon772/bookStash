bd5b23864873670f27d6686ca12b9844da483c91
Chip <Panopticon772@gmail.com>
2019-01-10 20:36:33 -0500

    Basic html, some js.

---
+++ app/assets/scripts/index.js
@@ -0,0 +1,20 @@
+// grab btns
+const getAddBtn = document.querySelector("#add-book");
+const getLibBtn = document.querySelector("#library");
+
+// array of book objects
+let library = [];
+
+// book objects
+
+// filter books by searchText or if owned (boolean)
+const filters = {
+    searchText: "",
+    owned: false
+}
+
+// event listeners
+
+// adds book to collection when clicked
+getAddBtn.addEventListener("click", () => {
+});
