ebec0b12778a382e2d82047aabb5ff017feea915
Chip <Panopticon772@gmail.com>
2019-01-26 21:04:00 -0500

    Refactoring process.

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -13,7 +13,7 @@ const getRemoveBtn = document.querySelector("#remove-book");
 // table selector
 const bookTable = document.querySelector("#book-table");
 
-// id remove input
+// remove id input
 const removeId = document.querySelector("#remove-id");
 
 // lib div
@@ -33,8 +33,11 @@ const closeModalBtn = document.querySelector(".close-modal");
 const yesBtn = document.querySelector("#delete-yes");
 const noBtn = document.querySelector("#delete-no");
 
-// let books initially be set equal to an empty array
-let books;
+// set books equal to local storage if it already exists, if not, set to empty array
+let booksArray = localStorage.getItem("books") ? JSON.parse(localStorage.getItem("books")) : [];
+
+// store books array as JSON string in localStorage
+const bookStorage = () => localStorage.setItem("books", JSON.stringify(booksArray));
 
 const capitalizeFirstLetter = (str) => {
     // convert to string and set all to lowercase
@@ -47,12 +50,6 @@ const clearData = () => {
     localStorage.clear();
 }
 
-// store books as JSON string in localStorage
-const bookStorage = () => localStorage.setItem("books", JSON.stringify(books));
-
-// retrieve books from localStorage
-const retrieveBooks = () => localStorage.getItem("books") ? JSON.parse(localStorage.getItem("books")) : [];
-
 // reset inputs
 const resetData = () => {
     getBookTitle.value = "";
@@ -60,77 +57,21 @@ const resetData = () => {
     getBookYear.value = "";
 }
 
-// display books as table
-const displayBooks = () => {
-    let arr = [];
-    const myBooks = JSON.parse(localStorage.getItem("books"));
-
-    // extract book object properties to use as table headers (id, title, author, year)
-    for (let i = 0; i < myBooks.length; i++) {
-        for (let key in myBooks[i]) {
-            if (arr.indexOf(key) === -1) {
-                arr.push(key);
-            }
-        }
-    }
-
-    // create table element
-    let table = document.createElement("table");
-    // set table id
-    table.setAttribute("id", "book-table");
-
-    // insert new row in table
-    let tr = table.insertRow(-1);
-
-    // create table headers from values, then append to table row
-    for (let i = 0; i < arr.length; i++) {
-        const th = document.createElement("th");
-        th.innerHTML = arr[i].toUpperCase();
-        tr.appendChild(th);
-    }
-
-    // add rows to table
-    for (let i = 0; i < myBooks.length; i++) {
-        tr = table.insertRow(-1);
-        // add table cells for each property value in obj
-        for (let j = 0; j < arr.length; j++) {
-            const tabCell = tr.insertCell(-1);
-            // if value is not a number convert first letter of every word to uppercase
-            if (isNaN(myBooks[i][arr[j]])) {
-                tabCell.innerHTML = capitalizeFirstLetter(myBooks[i][arr[j]]);
-            } else {
-                tabCell.innerHTML = myBooks[i][arr[j]];
-            }
-            
-        }
-    }
-
-    // set div to empty string
-    selectBooks.innerHTML = "";
-    // append table to div
-    selectBooks.appendChild(table);
-    
-}
-
 // give book an ID and increment by 1 on every loop
-const bookID = () => {
-    let id = 1;
-    for (let i = 0; i < books.length; i++) {
-        id++;
-    }
+// const bookID = () => {
+//     let id = 1;
+//     for (let i = 0; i < booksArray.length; i++) {
+//         id++;
+//     }
 
-    return id;
-}
+//     return id;
+// }
 
-const removeBook = (id) => {
-    // parse from local storage
-    const bookArray = JSON.parse(localStorage.getItem("books"));
-    // convert id from str to num
-    const num = Number(id);
-    //loop
-    for (let i = bookArray.length - 1; i >= 0; i--) {
-        if (bookArray[i] === num) {
-            delete bookArray[i];
+const removeBook = (title) => {
+    for (let i = 0; i < booksArray.length; i++) {
+        if (title === booksArray[i].title) {
+            booksArray.splice(booksArray[i], 1);
+            localStorage.setItem("books", JSON.stringify(booksArray));
         }
     }
 }
@@ -146,18 +87,8 @@ const windowOnClick = (e) => {
     }
 }
 
-// check if user already has storage, if so, display books, otherwise print "Add books!"
-if (localStorage.length > 0) {
-    displayBooks();
-} else {
-    // convert to open modal
-    console.log("Add some books to begin your library!");
-}
-
 getAddBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    // if books already exist in localStorage, get that array and push new book into it, otherwise create a new arr and add the book
-    books = retrieveBooks();
 
     if (getBookTitle.value === "") {
         alert("Please enter a title.");
@@ -165,37 +96,35 @@ getAddBtn.addEventListener("click", (e) => {
         alert("Please enter an author.");
     } else {
         // push user input values to array
-        books.push({
-            id: bookID(),
+        booksArray.push({
             title: getBookTitle.value,
             author: getBookAuthor.value,
             year: getBookYear.value
         });
 
-        // store array as JSON
         bookStorage();
 
-        displayBooks();
-
-        // reset user input
         resetData();
     }
 });
 
 // when clicked, removes correct book according to ID
-getRemoveBtn.addEventListener("click", () => {
-    removeBook(removeId.value)
+getRemoveBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    removeBook(removeId.value);
 });
 
-// remove table from screen and clear data from localStorage
-eraseBooksBtn.addEventListener("click", () => {
+// erase library from local storage
+eraseBooksBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     toggleModal();
 });
 
 window.addEventListener("click", windowOnClick);
 
 // clear data after confirmation
-yesBtn.addEventListener("click", () => {
+yesBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     clearData();
     toggleModal();
 });
@@ -205,4 +134,5 @@ noBtn.addEventListener("click", toggleModal);
 /* 1. need to add warning to erase button -> done 1/23/19
 2. fix remove btn
 3. filter books options, hightlight?
+4. display as table
  */
